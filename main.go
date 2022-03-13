75e1b1242c7c52c806f0a77388a281f1c8dc231e
killinsun <noneck.0311@gmail.com>
2022-03-13 15:47:53 +0900

    read/write file

--- main.go
+++ main.go
@@ -2,56 +2,41 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
-type Post struct {
-	Id int
-	Content string
-	Author string
-}
 
-// map = 連想配列、ハッシュ
-var PostById map[int] *Post // => {1: 0xc000098210, 2: 0xc...., 3: 0x....} 
-var PostsByAuthor map[string] []*Post // {"Sau Sheong": 0xc000098210, ,,,}
-
-func store(post Post) {
-	PostById[post.Id] = &post
-	PostsByAuthor[post.Author] = append(PostsByAuthor[post.Author], &post)
-}
 
 func main() {
 
-	// サイズの指定を省略しているので、最小の開始サイズを割り当てたスライスを返す
-	PostById = make(map[int] *Post)
-	PostsByAuthor = make(map[string][] *Post)
-
-	post1 := Post{Id: 1, Content: "Hello World!", Author: "Sau Sheong"}
-	fmt.Println(PostById)
-	fmt.Println(PostsByAuthor)
-	post2 := Post{Id: 2, Content: "Bonjour Monde!", Author: "Pierre"}
-	fmt.Println(PostById)
-	fmt.Println(PostsByAuthor)
-	post3 := Post{Id: 3, Content: "Hola Mundo!", Author:  "Pedro"}
-	post4 := Post{Id: 4, Content: "Greetings Earthlings!", Author: "Sau Sheong"}
-
-	fmt.Println(PostById[0])
-	fmt.Println(PostById)
-
-	store(post1)
-	store(post2)
-	store(post3)
-	store(post4)
-
-	fmt.Println(PostById)
-	fmt.Println(PostsByAuthor)
-	fmt.Println(PostById[1])
-	fmt.Println(PostById[2])
-
-	for _, post := range PostsByAuthor["Sau Sheong"] {
-		fmt.Println(post)
+	data := []byte("Hello World!\n")
+	err := ioutil.WriteFile("data1", data, 0644) // 0644 はパーミッション
+	if err != nil {
+		panic(err)
 	}
 
-	for _, post := range PostsByAuthor["Pedro"] {
-		fmt.Println(post)
-	}
+	read1, _ := ioutil.ReadFile("data1")
+	fmt.Print(string(read1))
+
+	// ------------------------------------------
+	// 構造体 File を使ったパターン
+	// ------------------------------------------
+
+	file1, _ := os.Create("data2") // 構造体Fileを取得
+	defer file1.Close() //
+
+	data2 := []byte("Hey! What's up?")
+
+	bytes, _ := file1.Write(data2)
+	fmt.Printf("Wrote %d bytes to file\n", bytes)
+
+	file2, _ := os.Open("data2")
+	defer file2.Close()
+
+	read2 := make([]byte, len(data2))
+	bytes, _ = file2.Read(read2)
+	fmt.Printf("Read %d bytes from file\n", bytes)
+	fmt.Println(string(read2))
+
 }
