ececfcb4c39369c2fdd0bc2de68a460e8277640e
killinsun <noneck.0311@gmail.com>
2022-03-14 06:55:53 +0900

    1:n and n:1 relations on go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -11,6 +12,14 @@ type Post struct {
 	Id int
 	Content string
 	Author string
+	Comments []Comment
+}
+
+type Comment struct {
+	Id int
+	Content string
+	Author string
+	Post *Post
 }
 
 var Db *sql.DB
@@ -41,9 +50,42 @@ func Posts(limit int) (posts []Post, err error) {
 	return
 }
 
+func (comment *Comment) Create() (err error) {
+	if comment.Post == nil {
+		err = errors.New("投稿が見つかりません")
+		return
+	}
+	statement := "insert into comments (content, author, post_id) values (?, ?, ?)"
+	stmt, err := Db.Prepare(statement)
+
+	defer stmt.Close()
+	result, err := stmt.Exec(comment.Content, comment.Author, comment.Post.Id)
+	lid, err := result.LastInsertId()
+
+	comment.Id = int(lid)
+
+	return
+}
+
 func GetPost(id int) (post Post, err error) {
 	post = Post{}
+	post.Comments = []Comment{}
 	err = Db.QueryRow("select id, content, author from posts where id = ?", id).Scan(&post.Id, &post.Content, &post.Author)
+
+	rows, err := Db.Query("select id, content, author from comments where post_id = ?", id)
+	if err != nil {
+		return
+	}
+
+	for rows.Next() {
+		comment := Comment{Post: &post}
+		err = rows.Scan(&comment.Id, &comment.Content, &comment.Author)
+		if err != nil {
+			return
+		}
+		post.Comments = append(post.Comments, comment)
+	}
+	rows.Close()
 	return
 }
 
@@ -79,16 +121,14 @@ func main() {
 
 	post.Create()
 
+	comment := Comment{Content: "いい投稿だね！", Author: "Joe", Post: &post}
+	comment.Create()
+
 	readPost, _ := GetPost(post.Id)
 	fmt.Println(readPost)
+	fmt.Println(readPost.Comments)
+	fmt.Println(readPost.Comments[0].Post)
 
-	readPost.Content = "Bonjour Monde!"
-	readPost.Author = "Pierre"
-	readPost.Update()
 
-	posts, _ := Posts(10)
-	fmt.Println(posts)
-
-	readPost.Delete()
 
 }
--- setup.sql
+++ setup.sql
@@ -1,5 +1,15 @@
+drop table if exists posts cascade;
+drop table if exists comments;
+
 create table posts (
 	id int primary key auto_increment,
 	content varchar(255),
 	author varchar(255)
-)
+);
+
+create table comments (
+	id int primary key auto_increment,
+	content varchar(255),
+	author varchar(255),
+	post_id int references posts(id)
+);
