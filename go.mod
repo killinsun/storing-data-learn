ca2081cbd6eb7cbc33d03412385d1be50dc13260
killinsun <noneck.0311@gmail.com>
2022-03-14 07:28:41 +0900

    try gorm

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.17
 
 require github.com/go-sql-driver/mysql v1.6.0
 
-require github.com/jmoiron/sqlx v1.3.4 // indirect
+require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jmoiron/sqlx v1.3.4 // indirect
+)
--- main.go
+++ main.go
@@ -2,90 +2,54 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
+	"github.com/jinzhu/gorm"
 )
 
 type Post struct {
 	Id int
 	Content string
-	AuthorName string `db: autho`
+	Author string `sql:"not null"`
+	Comments []Comment
+	CreatedAt time.Time
 }
 
-var Db *sqlx.DB
+type Comment struct {
+	Id int
+	Content string
+	Author string `sql:"not null"`
+	PostId int
+	CreatedAt time.Time
+}
+
+var Db *gorm.DB
 
 func init() {
 	var err error
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset-utf8", "test","test", "127.0.0.1", "gwp")
-	Db, err = sqlx.Open("mysql", dataSourceName)
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset-utf8&parseTime=true", "test","test", "127.0.0.1", "gwp")
+	Db, err = gorm.Open("mysql", dataSourceName)
 	if err != nil {
 		panic(err)
 	}
+	Db.AutoMigrate(&Post{}, &Comment{})
 }
-
-func Posts(limit int) (posts []Post, err error) {
-	rows, err := Db.Query("select id, content, author from posts limit ?", limit)
-	if err != nil {
-		return
-	}
-	for rows.Next() {
-		post := Post{}
-		err = rows.Scan(&post.Id, &post.Content, &post.AuthorName)
-		if err != nil {
-			return
-		}
-		posts = append(posts, post)
-	}
-	rows.Close()
-	return
-}
-
-func GetPost(id int) (post Post, err error) {
-	post = Post{}
-	err = Db.QueryRowx("select id, content, author from posts where id = ?", id).StructScan(&post)
-	if err != nil {
-		return
-	}
-	return
-}
-
-func (post *Post) Create() (err error) {
-	statement := "insert into posts (content, author) values (?, ?)"
-	stmt, err := Db.Prepare(statement)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	defer stmt.Close()
-	result, err := stmt.Exec(post.Content, post.AuthorName)
-	lid, err := result.LastInsertId()
-
-	post.Id = int(lid)
-	return 
-}
-
-func (post *Post) Update() (err error) {
-	_, err = Db.Exec("update posts set content = ?, author = ? where id = ?", post.Content, post.AuthorName, post.Id)
-	return
-}
-
-func (post *Post) Delete() (err error) {
-	_, err = Db.Exec("delete from posts where id = ?", post.Id)
-	return
-}
-
 
 func main() {
-	post := Post{Content: "Hello World!", AuthorName : "Sau Sheong"}
-
-	post.Create()
-
+	post := Post{Content: "Hello World!", Author: "Sau Sheong"}
 	fmt.Println(post)
 
-	readPost := Post{}
-	readPost, _ = GetPost(post.Id)
-	fmt.Println(readPost)
+	Db.Create(&post)
+	fmt.Println(post)
+
+	comment := Comment{Content: "良い投稿だね！", Author: "Joe"}
+	Db.Model(&post).Association("Comments").Append(comment)
+
+	var readPost Post
+	Db.Where("author = ?", "Sau Sheong").First(&readPost)
+	var comments []Comment
+	Db.Model(&readPost).Related(&comments)
+	fmt.Println(comments[0])
 
 }
