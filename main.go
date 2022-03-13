85c5867e05b5a128ecfa5525da78fbd7804975a0
killinsun <noneck.0311@gmail.com>
2022-03-13 15:34:41 +0900

    added comment

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ type Post struct {
 	Author string
 }
 
-var PostById map[int] *Post
-var PostsByAuthor map[string] []*Post
+// map = 連想配列、ハッシュ
+var PostById map[int] *Post // => {1: 0xc000098210, 2: 0xc...., 3: 0x....} 
+var PostsByAuthor map[string] []*Post // {"Sau Sheong": 0xc000098210, ,,,}
 
 func store(post Post) {
 	PostById[post.Id] = &post
@@ -20,19 +21,29 @@ func store(post Post) {
 
 func main() {
 
+	// サイズの指定を省略しているので、最小の開始サイズを割り当てたスライスを返す
 	PostById = make(map[int] *Post)
 	PostsByAuthor = make(map[string][] *Post)
 
 	post1 := Post{Id: 1, Content: "Hello World!", Author: "Sau Sheong"}
+	fmt.Println(PostById)
+	fmt.Println(PostsByAuthor)
 	post2 := Post{Id: 2, Content: "Bonjour Monde!", Author: "Pierre"}
+	fmt.Println(PostById)
+	fmt.Println(PostsByAuthor)
 	post3 := Post{Id: 3, Content: "Hola Mundo!", Author:  "Pedro"}
 	post4 := Post{Id: 4, Content: "Greetings Earthlings!", Author: "Sau Sheong"}
+
+	fmt.Println(PostById[0])
+	fmt.Println(PostById)
 
 	store(post1)
 	store(post2)
 	store(post3)
 	store(post4)
 
+	fmt.Println(PostById)
+	fmt.Println(PostsByAuthor)
 	fmt.Println(PostById[1])
 	fmt.Println(PostById[2])
 
